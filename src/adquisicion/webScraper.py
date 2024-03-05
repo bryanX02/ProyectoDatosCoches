7157a69f3f76be3e0ba17701fc249edd29eae840
pabrodsos <123556590+pabrodsos@users.noreply.github.com>
2024-03-05 01:11:15 +0100

    Autor: Pablo -Solucionado problema de syntax

--- src/adquisicion/webScraper.py
+++ src/adquisicion/webScraper.py
@@ -23,11 +23,19 @@ import time
 # Función que devuelve un drive para el scraping con las configuraciones aplicadas
 def web_driver():
     options = webdriver.ChromeOptions()
-    options.add_argument('--no-sandbox')  # Deshabilita el limitador
-    options.add_argument('--headless')  # Deshabilita la ventana y se realiza en un segundo plano
-    options.add_argument('--disable-gpu')  # Deshabilita la GPU para evitar problemas en algunas webs
-    options.add_argument("--window-size=1920, 2160")  # Tamaño de la ventana (en segundo plano)
-    options.add_argument('--disable-dev-shm-usage')  # Deshabilita los archivos temporales
+    options.add_argument("--no-sandbox")  # Deshabilita el limitador
+    options.add_argument(
+        "--headless"
+    )  # Deshabilita la ventana y se realiza en un segundo plano
+    options.add_argument(
+        "--disable-gpu"
+    )  # Deshabilita la GPU para evitar problemas en algunas webs
+    options.add_argument(
+        "--window-size=1920, 2160"
+    )  # Tamaño de la ventana (en segundo plano)
+    options.add_argument(
+        "--disable-dev-shm-usage"
+    )  # Deshabilita los archivos temporales
     return webdriver.Chrome(options=options)
 
 
@@ -49,7 +57,9 @@ def scraper(driver, num_scroll):
         time.sleep(3)
 
         # Se busca el botón y se le da clíck
-        btn = driver.find_element(By.CLASS_NAME, "button___2R6qU.size-sm___3TKQS.default___1FRAY")
+        btn = driver.find_element(
+            By.CLASS_NAME, "button___2R6qU.size-sm___3TKQS.default___1FRAY"
+        )
         btn.click()  # Se aceptan las cookies
 
     except:
@@ -63,13 +73,14 @@ def scraper(driver, num_scroll):
     screen_height = driver.execute_script("return window.screen.height;")
 
     while i <= n:
-
         # Se realiza el scroll para la siguiente página
         driver.execute_script(f"window.scrollTo(0, {screen_height * i})")
         time.sleep(scroll_pause_time)  # Tiempo de carga de la siguiente página
-        scraperSoup = BeautifulSoup(driver.page_source, 'html.parser')
+        scraperSoup = BeautifulSoup(driver.page_source, "html.parser")
         # Se sacan los coches de la página actua
-        pagVehiculos = scraperSoup.find_all('a', class_='link___2Maxt color-inherit___SyKXO decoration-none___1IENu')
+        pagVehiculos = scraperSoup.find_all(
+            "a", class_="link___2Maxt color-inherit___SyKXO decoration-none___1IENu"
+        )
         # Se añaden al conjunto
         for vehiculo in pagVehiculos:
             vehiculos.add(vehiculo)
@@ -87,70 +98,69 @@ def scraper(driver, num_scroll):
 def agregar_autohero(link):
     return "https://www.autohero.com" + link
 
+
 def obtener_info_vehiculo_1(vehiculo):
-    infoExtra = vehiculo.get('href')
-    nombre_vehiculo = vehiculo.find('h2', class_='title___1TYYE').text.strip()
-    precioFinan = vehiculo.find('div', class_='price___uwAkj').text.strip()
-    modelo = vehiculo.find('h3', class_='subtitle___1ZA66').text.strip()
+    infoExtra = vehiculo.get("href")
+    nombre_vehiculo = vehiculo.find("h2", class_="title___1TYYE").text.strip()
+    precioFinan = vehiculo.find("div", class_="price___uwAkj").text.strip()
+    modelo = vehiculo.find("h3", class_="subtitle___1ZA66").text.strip()
     link = agregar_autohero(infoExtra)
 
     # Realizar solicitud al enlace principal
     response_vehiculo = requests.get(link)
     html_vehiculo = response_vehiculo.text
-    soup_vehiculo = BeautifulSoup(html_vehiculo, 'html.parser')
+    soup_vehiculo = BeautifulSoup(html_vehiculo, "html.parser")
 
     # Extraer información adicional del enlace
-    descripcion_vehiculo = soup_vehiculo.find_all('div', class_='listItemText___uoGhx')
-    specs = soup_vehiculo.find_all('div', class_='item___3Uae0')
+    descripcion_vehiculo = soup_vehiculo.find_all("div", class_="listItemText___uoGhx")
+    specs = soup_vehiculo.find_all("div", class_="item___3Uae0")
 
     # Inicializar variables
-    primera_matriculacion = kilometraje = carburante = transmision = potencia = traccion = None
-    precio2 = soup_vehiculo.find('div', class_='vehiclePriceContainer___jwSn_')
+    primera_matriculacion = (
+        kilometraje
+    ) = carburante = transmision = potencia = traccion = None
+    precio2 = soup_vehiculo.find("div", class_="vehiclePriceContainer___jwSn_")
     if precio2:
-        precio = precio2.find('p', class_='vehiclePrice___1uUmJ').text.strip()
+        precio = precio2.find("p", class_="vehiclePrice___1uUmJ").text.strip()
     else:
         precio = precioFinan
 
     # Iterar sobre la información extraída
     for elemento in descripcion_vehiculo:
-        titulo = elemento.find('h5', class_='listItemTitle___2CQBv').text.strip()
-        valor = elemento.find('span', class_='listItemValue___1IWSE').text.strip()
+        titulo = elemento.find("h5", class_="listItemTitle___2CQBv").text.strip()
+        valor = elemento.find("span", class_="listItemValue___1IWSE").text.strip()
 
         # Asignar valores a las variables correspondientes
-        if titulo == 'Primera matriculación':
+        if titulo == "Primera matriculación":
             primera_matriculacion = valor
-        elif titulo == 'Kilometraje':
+        elif titulo == "Kilometraje":
             kilometraje = valor
-        elif titulo == 'Carburante':
+        elif titulo == "Carburante":
             carburante = valor
-        elif titulo == 'Transmisión':
+        elif titulo == "Transmisión":
             transmision = valor
-        elif titulo == 'Potencia':
+        elif titulo == "Potencia":
             potencia = valor
-        elif titulo == 'Tracción':
+        elif titulo == "Tracción":
             traccion = valor
 
-
-   # Iterar sobre la información extra
+    # Iterar sobre la información extra
     for elemento in specs:
-      try:
-        titulo = elemento.find('div', class_='itemTitle___3GH8k').text.strip()
-      except:
-          continue
-      try:
-        valor = elemento.find('div', class_='body___2uId6').text.strip()
-      except:
-          # Si no se encuentra el elemento 'body___2uId6', se asigna None o se continua con el siguiente elemento
-          valor = np.nan
+        try:
+            titulo = elemento.find("div", class_="itemTitle___3GH8k").text.strip()
+        except:
+            continue
+        try:
+            valor = elemento.find("div", class_="body___2uId6").text.strip()
+        except:
+            # Si no se encuentra el elemento 'body___2uId6', se asigna None o se continua con el siguiente elemento
+            valor = np.nan
 
-      # Asignar valores a las variables correspondientes
-      if titulo == 'Cilindrada':
-          cilindrada = valor
-      elif titulo == 'Coche accidentado y reparado':
-          coche_accidentado = valor
-
-
-
+        # Asignar valores a las variables correspondientes
+        if titulo == "Cilindrada":
+            cilindrada = valor
+        elif titulo == "Coche accidentado y reparado":
+            coche_accidentado = valor
 
     # Imprimir o almacenar la información según sea necesario
     # print(f"Nombre del vehículo: {nombre_vehiculo}")
@@ -167,30 +177,28 @@ def obtener_info_vehiculo_1(vehiculo):
 
     # Crear un DataFrame con una sola fila
     data = {
-        'Modelo': nombre_vehiculo,
-        'Precio(€)': precio,
-        'Adicional': modelo,
+        "Modelo": nombre_vehiculo,
+        "Precio(€)": precio,
+        "Adicional": modelo,
         #'Enlace Autohero': link,
-        'Primera matriculación': primera_matriculacion,
-        'Kilometraje(Km)': kilometraje,
-        'Carburante': carburante,
-        'Transmisión': transmision,
-        'Potencia(Cv)': potencia,
-        'Tracción': traccion,
-        'Cilindrada(Cc)': cilindrada,
-        'Accidentado': coche_accidentado
+        "Primera matriculación": primera_matriculacion,
+        "Kilometraje(Km)": kilometraje,
+        "Carburante": carburante,
+        "Transmisión": transmision,
+        "Potencia(Cv)": potencia,
+        "Tracción": traccion,
+        "Cilindrada(Cc)": cilindrada,
+        "Accidentado": coche_accidentado,
     }
     df = pd.DataFrame([data])
 
     return df
 
 
-
 # Ejecución
 def main():
-
     # Fuente de los datos
-    url = 'https://www.autohero.com/es/search/'
+    url = "https://www.autohero.com/es/search/"
 
     # Driver (navegador) que se empleará
     driver = web_driver()
@@ -204,9 +212,13 @@ def main():
     df_resultado = pd.DataFrame()
     for vehiculo in vehiculos:
         try:
-            df_resultado = pd.concat([df_resultado, obtener_info_vehiculo_1(vehiculo)], ignore_index=True)
+            df_resultado = pd.concat(
+                [df_resultado, obtener_info_vehiculo_1(vehiculo)], ignore_index=True
+            )
         except:
-            print(f"Excepción de datos en {vehiculo.get('href')}")
+            print(
+                f"Excepción de datos en {vehiculo.get('href')}"
+            )  # solucionado problema comillas
 
     print(df_resultado)
 
