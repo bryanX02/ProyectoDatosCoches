c025cd0df667eb1238a15823b2e0130b35f8fec1
02bryanX <bryan.xavier.2002@gmail.com>
2024-04-15 18:36:25 +0200

    MLFlow

--- src/modelos/RedesNeuronales.py
+++ src/modelos/RedesNeuronales.py
@@ -7,12 +7,17 @@
 
 # Librerias
 import pandas as pd
+from scipy.stats import loguniform
 from sklearn.neural_network import MLPRegressor
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV, KFold
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import mean_squared_error, mean_absolute_error, accuracy_score
 from tqdm import tqdm
 import numpy as np
+import os
+import mlflow
+import mlflow.sklearn
+
 
 # Función que separa en train y test empleando la libreria correspondiente de Scikit-Learn
 def splitTrainTest(X, y, testSize=0.3, randomState=42):
@@ -38,12 +43,82 @@ def transformData(datos):
 
     return dataTrans
 
+def ajusteHiperparametros(X_train, y_train, model):
+
+    # Definimos los hiperparámetros que se combinaran. Son 27 (ya que en este modelo se requieren muchos recursos)
+    param_grid = {
+        'hidden_layer_sizes': [(50, 25), (100, 50), (150, 100)],
+        'activation': ['relu', 'tanh', 'logistic'],
+        'alpha': [0.0001, 0.001, 0.01]
+    }
+
+    # Definición de hiperparámetros a probar
+    param_range = {
+        'hidden_layer_sizes': [(i, j) for i in range(50, 151, 50) for j in range(25, 101, 25)],
+        'activation': ['relu', 'tanh', 'logistic'],
+        'alpha': loguniform(1e-4, 1e-2),
+    }
+
+    # Definimos la validación cruzada
+    cv = KFold(n_splits=5, shuffle=True, random_state=42)
+
+    # Búsqueda en rejilla
+    grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=cv)
+    grid_search.fit(X_train, y_train)
+
+    # Resultados de la búsqueda en rejilla
+    print("Mejores hiperparámetros encontrados mediante búsqueda en rejilla:")
+    print(grid_search.best_params_)
+    print("MSE en entrenamiento:", -grid_search.best_score_)
+
+    # Búsqueda aleatoria
+    random_search = RandomizedSearchCV(estimator=model, param_distributions=param_range, n_iter=27, cv=cv)
+    random_search.fit(X_train, y_train)
+
+    # Resultados de la búsqueda aleatoria
+    print("\nMejores hiperparámetros encontrados mediante búsqueda aleatoria:")
+    print(random_search.best_params_)
+    print("MSE en entrenamiento:", -random_search.best_score_)
 
 def analisisResultado(y_test, y_pred):
     mae = mean_absolute_error(y_test, y_pred)
     mse = mean_squared_error(y_test, y_pred)
     rmse = np.sqrt(y_test, y_pred)
     print("\nRMSE: ", rmse, "\n\nMAE: ", mae)
+
+def MLFlow(X_train, y_train, X_test, y_test):
+
+    model = MLPRegressor(max_iter=1000)
+
+    # Definición de hiperparámetros a probar
+    param_range = {
+        'hidden_layer_sizes': [(i, j) for i in range(50, 151, 50) for j in range(25, 101, 25)],
+        'activation': ['relu', 'tanh', 'logistic'],
+        'alpha': loguniform(1e-4, 1e-2),
+    }
+
+    # Definimos la validación cruzada
+    cv = KFold(n_splits=5, shuffle=True, random_state=42)
+
+    random_search = RandomizedSearchCV(model, param_distributions=param_range, cv=cv, n_iter=10)
+
+    # Configurar MLFlow para usar SQLite como backend
+    os.environ['MLFLOW_TRACKING_URI'] = 'sqlite:///mlflow.db'
+
+    # Creamos (o elegimos nuestro experimento)
+    mlflow.set_experiment("Random search")
+
+    # Iniciar una nueva ejecución de MLFlow
+    with mlflow.start_run():
+        # Realizar la búsqueda de hiperparámetros
+        random_search.fit(X_train, y_train)
+
+        # Registrar el modelo en MLFlow
+        mlflow.sklearn.log_model(random_search.best_estimator_, "model")
+
+        # Registrar los hiperparámetros y la puntuación del mejor modelo
+        mlflow.log_params(random_search.best_params_)
+        mlflow.log_metric("accuracy", accuracy_score(y_test, random_search.predict(X_test)))
 
 def main():
 
@@ -61,8 +136,12 @@ def main():
     # Con los datos ya listos, separamos entre train y test
     X_train, X_test, y_train, y_test = splitTrainTest(X, y)
 
-    # Y ahora ya generamos y entrenamos el modelo
-    reg = MLPRegressor(hidden_layer_sizes=(100, 50), random_state=42, max_iter=1000)
+    # Generamos el modelo y estudiamos que hiperparámetros son los óptimos
+    reg = MLPRegressor(max_iter=1000)
+    ajusteHiperparametros(X_train, y_train, reg)
+
+    # Y ahora ya ajustamos el modelo con los hiperparámetros obtenidos
+    reg = MLPRegressor(hidden_layer_sizes=(150, 100), alpha=0.0001, activation='relu', random_state=42)
 
     # Inicializamos la barra de progreso
     pbar = tqdm(total=reg.max_iter, desc="Entrenando modelo", position=0, leave=True)
@@ -88,6 +167,9 @@ def main():
     precisionTest = reg.score(X_test.values, y_test.values.ravel())
     print("Precisión del modelo (R^2):", precisionTest)
     analisisResultado(y_test, y_pred)
+
+    # Por último registramos el experimento con MLFlow
+    MLFlow(X_train, y_train, X_test, y_test)
 
 
 if __name__ == "__main__":
