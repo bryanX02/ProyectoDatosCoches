18327b672451d7c4e5886a7f56a2334246e25cf7
02bryanX <bryan.xavier.2002@gmail.com>
2024-04-16 19:17:42 +0200

    MLFlow de Redes Neuronales terminado

--- src/modelos/RedesNeuronales.py
+++ src/modelos/RedesNeuronales.py
@@ -45,10 +45,10 @@ def transformData(datos):
 
 def ajusteHiperparametros(X_train, y_train, model):
 
-    # Definimos los hiperparámetros que se combinaran. Son 27 (ya que en este modelo se requieren muchos recursos)
+    # Definimos los hiperparámetros que se combinaran. Son 18 (ya que en este modelo se requieren muchos recursos)
     param_grid = {
         'hidden_layer_sizes': [(50, 25), (100, 50), (150, 100)],
-        'activation': ['relu', 'tanh', 'logistic'],
+        'activation': ['relu', 'tanh'],
         'alpha': [0.0001, 0.001, 0.01]
     }
 
@@ -86,39 +86,25 @@ def analisisResultado(y_test, y_pred):
     rmse = np.sqrt(y_test, y_pred)
     print("\nRMSE: ", rmse, "\n\nMAE: ", mae)
 
-def MLFlow(X_train, y_train, X_test, y_test):
-
-    model = MLPRegressor(max_iter=1000)
-
-    # Definición de hiperparámetros a probar
-    param_range = {
-        'hidden_layer_sizes': [(i, j) for i in range(50, 151, 50) for j in range(25, 101, 25)],
-        'activation': ['relu', 'tanh', 'logistic'],
-        'alpha': loguniform(1e-4, 1e-2),
-    }
-
-    # Definimos la validación cruzada
-    cv = KFold(n_splits=5, shuffle=True, random_state=42)
-
-    random_search = RandomizedSearchCV(model, param_distributions=param_range, cv=cv, n_iter=10)
+def MLFlow(X_train, y_train, X_test, y_test, search, title):
 
     # Configurar MLFlow para usar SQLite como backend
     os.environ['MLFLOW_TRACKING_URI'] = 'sqlite:///mlflow.db'
 
     # Creamos (o elegimos nuestro experimento)
-    mlflow.set_experiment("Random search")
+    mlflow.set_experiment(title)
 
     # Iniciar una nueva ejecución de MLFlow
     with mlflow.start_run():
         # Realizar la búsqueda de hiperparámetros
-        random_search.fit(X_train, y_train)
+        search.fit(X_train, y_train)
 
         # Registrar el modelo en MLFlow
-        mlflow.sklearn.log_model(random_search.best_estimator_, "model")
+        mlflow.sklearn.log_model(search.best_estimator_, "model")
 
         # Registrar los hiperparámetros y la puntuación del mejor modelo
-        mlflow.log_params(random_search.best_params_)
-        mlflow.log_metric("accuracy", accuracy_score(y_test, random_search.predict(X_test)))
+        mlflow.log_params(search.best_params_)
+        mlflow.log_metric("MAE", mean_absolute_error(y_test, search.predict(X_test)))
 
 def main():
 
@@ -168,8 +154,28 @@ def main():
     print("Precisión del modelo (R^2):", precisionTest)
     analisisResultado(y_test, y_pred)
 
+    # COMENTAR LO SIGUIENTE SI NO SE QUIERE GENERAR EL MLFLOW:
+
     # Por último registramos el experimento con MLFlow
-    MLFlow(X_train, y_train, X_test, y_test)
+    # Definimos los hiperparámetros que se combinaran. Son 18 (ya que en este modelo se requieren muchos recursos)
+    param_grid = {
+        'hidden_layer_sizes': [(50, 25), (100, 50), (150, 100)],
+        'activation': ['relu', 'tanh'],
+        'alpha': [0.0001, 0.001, 0.01]
+    }
+
+    # Definición de hiperparámetros a probar
+    param_range = {
+        'hidden_layer_sizes': [(i, j) for i in range(50, 151, 50) for j in range(25, 101, 25)],
+        'activation': ['relu', 'tanh', 'logistic'],
+        'alpha': loguniform(1e-4, 1e-2),
+    }
+
+    model = MLPRegressor(max_iter=1000)
+    random_search = GridSearchCV(model, param_grid=param_grid, cv=3)
+    MLFlow(X_train, y_train, X_test, y_test, random_search, "Grid Search")
+    random_search = RandomizedSearchCV(model, param_distributions=param_range, cv=3, n_iter=10)
+    MLFlow(X_train, y_train, X_test, y_test, random_search, "Random Search")
 
 
 if __name__ == "__main__":
