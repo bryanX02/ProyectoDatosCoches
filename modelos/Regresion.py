7a57a6c95df9c557f77ff8fe8fe718a0bdd964c7
pabrodsos <pablro21@ucm.es>
2024-04-28 23:37:15 +0200

    Update Regresion.py

--- modelos/Regresion.py
+++ modelos/Regresion.py
@@ -13,12 +13,6 @@ def apply_pca(X, n_components=20):
     X_pca = pca.fit_transform(X)
     return X_pca, pca
 
-def calculate_vif(X_pca):
-    vif_data = pd.DataFrame()
-    vif_data['VIF'] = [variance_inflation_factor(X_pca, i) for i in range(X_pca.shape[1])]
-    vif_data['feature'] = range(X_pca.shape[1])
-    return vif_data
-
 
 def ajusteHiperparametros(X_train, y_train, X_test, y_test, model, mlflow = False):
 
@@ -58,7 +52,6 @@ def main():
     # Transformación y PCA
     X_transformed, scaler = transformData(X)
     X_pca, pca = apply_pca(X_transformed)
-    vif_data = calculate_vif(pd.DataFrame(X_pca))
 
     X_train, X_test, y_train, y_test = splitTrainTest(X_pca, y)
 
@@ -66,12 +59,12 @@ def main():
     ajusteHiperparametros(X_train, y_train, X_test, y_test, reg, mlflow=True)
 
     # Uso de los mejores parámetros (se debe extraer de GridSearch si necesario)
-    reg = LinearRegression(fit_intercept=False, copy_X= True)
+    reg = LinearRegression(fit_intercept=True, copy_X=False)
     reg.fit(X_train, y_train)
     y_pred = reg.predict(X_test)
     print("Precisión del modelo (R^2):", reg.score(X_test, y_test))
     analisisResultado(y_test, y_pred)
-    plotModelo(y_test, y_pred, "Linear Regression")
+    plotModelo(y_test, y_pred, "Linear Regression with PCA")
 
 
 if __name__ == "__main__":
